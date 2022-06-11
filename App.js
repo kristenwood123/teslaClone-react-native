6c3218de7114af9afdac9efa1401399886900b6f
Kristen <kristenwood123@gmail.com>
2022-06-11 15:34:29 -0700

    added car list and flatlist

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import CarItem from "./components/CarItem";
+import CarsList from "./components/CarsList";
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem
-        name={"Model X"}
-        tagline={"Order Online For"}
-        taglineCTA={"Touchless delivery"}
-        image={require("./assets/images/Model3.jpeg")}
-      />
+      <CarsList />
       <StatusBar style="auto" />
     </View>
   );
