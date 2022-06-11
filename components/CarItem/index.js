4cee3679ba577ac74dc3a8d1ecdbe92b47f0c143
Kristen <kristenwood123@gmail.com>
2022-06-11 15:38:54 -0700

    completed car item component

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -11,7 +11,7 @@ const CarItem = (props) => {
       <View style={styles.titles}>
         <Text style={styles.title}>{name}</Text>
         <Text style={styles.subtitle}>
-          {tagline} <Text style={styles.taglineCTA}>{taglineCTA}</Text>
+          {tagline} <Text style={styles.subtitleCTA}>{taglineCTA}</Text>
         </Text>
       </View>
 
